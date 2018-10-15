5a17a78d35f230d27e14fab0c2cae254c877bf6e
Leonardo Porto <leonardoporto159ster@gmail.com>
2018-10-15 00:00:47 -0300

    Corrigida a contagem dos padroes
    Retestar a contagem, posso ter esquecido de testar algum caso. Mas tentei com mais de uma milha, e com mais de uma palavra por linha separadas por espaços

--- paralela/sequential.cpp
+++ paralela/sequential.cpp
@@ -1,7 +1,7 @@
 //source: https://www.geeksforgeeks.org/wildcard-pattern-matching/
 
-// C++ program to implement wildcard 
-// pattern matching algorithm 
+// C++ program to implement wildcard
+// pattern matching algorithm
 #include <bits/stdc++.h>
 #include <iostream>
 #include <fstream>
@@ -12,67 +12,76 @@ using namespace std;
 ofstream patternFile;
 ofstream textFile;
 
-// Function that matches input str with given wildcard pattern 
-int strmatch(string str, string pattern, int n, int m) 
+// Function that matches input str with given wildcard pattern
+int strmatch(string str, string pattern, int n, int m)
 {
-    int matchCounter = 0; 
-    // empty pattern can only match with empty string 
-    if (m == 0) 
-        return (n == 0); 
-  
-    // lookup table for storing results of subproblems 
-    bool lookup[n + 1][m + 1]; 
-  
-    // initailze lookup table to false 
-    memset(lookup, false, sizeof(lookup)); 
-  
-    // empty pattern can match with empty string 
-    lookup[0][0] = true; 
-  
-    // Only '*' can match with empty string 
-    for (int j = 1; j <= m; j++) 
-        if (pattern[j - 1] == '*') 
-            lookup[0][j] = lookup[0][j - 1]; 
-  
-    // fill the table in bottom-up fashion 
-    for (int i = 1; i <= n; i++) 
-    { 
-        for (int j = 1; j <= m; j++) 
-        { 
-            // Two cases if we see a '*' 
-            // a) We ignore ‘*’ character and move to next  character in the pattern, 
-            //     i.e., ‘*’ indicates an empty sequence. 
-            // b) '*' character matches with ith character in input 
-            if (pattern[j - 1] == '*'){ 
-                lookup[i][j] = lookup[i][j - 1] || lookup[i - 1][j];
-                matchCounter++;  
-            } 
-  
-            // Current characters are considered as matching in two cases 
-            // (a) current character of pattern is '?' 
-            // (b) characters actually match 
-            else if (pattern[j - 1] == '?' || str[i - 1] == pattern[j - 1]) 
-                lookup[i][j] = lookup[i - 1][j - 1]; 
-  
-            // If characters don't match 
-            else lookup[i][j] = false; 
-        } 
-    } 
+    int matchCounter = 0;
+    // empty pattern can only match with empty string
+    if (m == 0)
+        return (n == 0);
+
+    // lookup table for storing results of subproblems
+    bool lookup[n + 1][m + 1];
+
+    // initailze lookup table to false
+    memset(lookup, false, sizeof(lookup));
+
+    // empty pattern can match with empty string
+    lookup[0][0] = true;
+
+    // Only '*' can match with empty string
+    for (int j = 1; j <= m; j++)
+        if (pattern[j - 1] == '*')
+            lookup[0][j] = lookup[0][j - 1];
+
+    int j;
+    // fill the table in bottom-up fashion
+    for (int i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= m; j++)
+        {
+            // Two cases if we see a '*'
+            // a) We ignore ‘*’ character and move
+            //    to next  character in the pattern,
+            //     i.e., ‘*’ indicates an empty sequence.
+            // b) '*' character matches with ith
+            //     character in input
+            if (pattern[j - 1] == '*')
+                lookup[i][j] = lookup[i][j - 1] ||
+                               lookup[i - 1][j];
+
+            // Current characters are considered as
+            // matching in two cases
+            // (a) current character of pattern is '?'
+            // (b) characters actually match
+            else if (pattern[j - 1] == '?' ||
+                    str[i - 1] == pattern[j - 1])
+                lookup[i][j] = lookup[i - 1][j - 1];
+
+            // If characters don't match
+            else lookup[i][j] = false;
+        }
+        if(str[i-1] == ' ' && lookup[i-2][j-2])
+            matchCounter++;
+    }
+    if(lookup[n][m])
+        matchCounter++;
     return matchCounter;
-    //return lookup[n][m]; 
+    //return lookup[n][m];
 }
 
-string getAllLines(string nameOfFile){
+vector<string> getAllLines(string nameOfFile){
     string str;
-    ifstream myFile(nameOfFile);
-    string linesOfFile;
-    while (std::getline(myFile, str))
+    ifstream myFile(nameOfFile, std::ifstream::in);
+    vector<string> linesOfFile;
+    while (getline(myFile, str))
     {
-        linesOfFile += str;
-        linesOfFile.push_back('\n');
+        linesOfFile.push_back(str);
+        //linesOfFile.push_back('\n');
     }
+
     myFile.close();
-    return str;
+    return linesOfFile;
 }
 
 void closeFiles()
@@ -80,25 +89,31 @@ void closeFiles()
     patternFile.close();
     textFile.close();
 }
-  
-int main() 
-{ 
-    string patternFile_contents = getAllLines("pattern.txt");
-    string textFile_contents = getAllLines("text.txt");    
-    
+
+int main()
+{
+    vector<string> patternFile_contents = getAllLines("pattern.txt");
+    vector<string> textFile_contents = getAllLines("text.txt");
+
     /*
-    if (strmatch(textFile_contents, patternFile_contents, textFile_contents.size(), patternFile_contents.size())) 
-        cout << "Yes" << endl; 
+    if (strmatch(textFile_contents, patternFile_contents, textFile_contents.size(), patternFile_contents.size()))
+        cout << "Yes" << endl;
     else
-        cout << "No" << endl; 
+        cout << "No" << endl;
     */
     int counter = 0;
-    counter = strmatch(textFile_contents, patternFile_contents, textFile_contents.size(), patternFile_contents.size());
+    for(string line : textFile_contents){
+        for(string pattern : patternFile_contents){
+            counter += strmatch(line, pattern, line.length(), pattern.length());
+        }
+    }
 
-    std::cout << "Number of matches: " << counter << endl; 
-    
+    //counter = strmatch(textFile_contents, patternFile_contents, textFile_contents.size(), patternFile_contents.size());
+
+    std::cout << "Number of matches: " << counter << endl;
+
     closeFiles();
 
     return 0;
-    
-} 
+
+}
