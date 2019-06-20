39c406080b21145f1503d6470b10d183a5ddb1e7
mbritzke <matheusbritzke@gmail.com>
2019-06-20 15:02:39 -0300

    Função create_tcp_fields

--- labredes/port_scanning.py
+++ labredes/port_scanning.py
@@ -11,4 +11,32 @@ class TCPPacket:
         self.dst_ip = dst
         self.data   = data
         self.raw = None
-        self.create_tcp_feilds()
+        self.create_tcp_fields()
+
+    def create_tcp_fields(self):
+        self.tcp_src = self.sport   # Source port
+        self.tcp_dst = self.dport   # Destination Port
+        self.tcp_seq = 0            # Sequence Number
+        self.tcp_ack_seq = 0        # Acknowledgement Number
+        self.tcp_hdr_len = 80       # Header Length
+        
+        tcp_flags_rsv = (0 << 9)
+        tcp_flags_noc = (0 << 8)
+        tcp_flags_cwr = (0 << 7)
+        tcp_flags_ecn = (0 << 6)
+        tcp_flags_urg = (0 << 5)
+        tcp_flags_ack = (0 << 4)
+        tcp_flags_psh = (0 << 3)
+        tcp_flags_rst = (0 << 2)
+        tcp_flags_syn = (1 << 1)
+        tcp_flags_fin = (0)
+
+        self.tcp_flags = tcp_flags_rsv + tcp_flags_noc + tcp_flags_cwr + \
+                tcp_flags_ecn + tcp_flags_urg + tcp_flags_ack + \
+                tcp_flags_psh + tcp_flags_rst + tcp_flags_syn + tcp_flags_fin
+
+        self.tcp_wdw = socket.htons (5840)  # Window Size
+        self.tcp_chksum = 0                 # CheckSum
+        self.tcp_urg_ptr = 0                # Urgent Pointer
+        
+        return
